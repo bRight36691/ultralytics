48e84cf45352bb2ed45bda859e3a8172eade36fa
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2025-01-14 22:42:21 +0700

    debug layer

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -966,6 +966,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 with contextlib.suppress(ValueError):
                     args[j] = locals()[a] if a in locals() else ast.literal_eval(a)
         n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
+        print(m)
         if m in {
             Classify,
             Conv,
