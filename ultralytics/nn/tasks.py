6782b61507253651b70b6f1e14ecba35ba6b188c
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2025-03-24 00:59:10 +0700

    fix

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1074,7 +1074,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is CBFuse:
             c2 = ch[f[-1]]
         elif m is CDCB or m is DBRA :
-            args = [c1, c2]
+            args = [ch[f], args[0]]
         else:
             c2 = ch[f]
         print(args)
